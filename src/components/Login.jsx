59d06f4554df880fae7c7004840215f910757400
luzmarina <74207825+luzmarin@users.noreply.github.com>
2025-10-28 17:29:12 +0100

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -36,7 +36,7 @@ export const Login = ({ items }) => {
 
         //Se envían los datos al backend con fetch
         try {
-            const response = await fetch("http://localhost:3000/api/v1/login", {
+            const response = await fetch("http://localhost:3000/api/v1/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
